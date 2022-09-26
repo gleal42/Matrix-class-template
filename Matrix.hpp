256ad2bdba558cb333e46dca1c023af023aa4919
gleal42 <gonaloleal@gmail.com>
2022-09-26 22:17:13 +0100

    Matrix multiplication and transposition.

--- Matrix.hpp
+++ Matrix.hpp
@@ -5,6 +5,7 @@
 #include <list>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 template <size_t Y, size_t X>
 class Matrix
@@ -30,6 +31,31 @@ public:
 		}
 		std::cout << *this;
 	}
+
+	/**
+	 * @brief 
+	 * 1 2 3
+	 * 4 5 6
+	 * 
+	 * to
+	 * 
+	 * 1 4
+	 * 2 5
+	 * 3 6
+	 * @return Matrix<X, Y> 
+	 */
+
+	Matrix<X, Y> transposed( void ) const
+	{
+		Matrix<X, Y> t_matrix;
+		// typename Matrix<X, Y>::iterator t_it = t_matrix.begin();
+		for (size_t y = 0; y != this->_data.size(); y++) {
+			for (size_t x = 0; x != this->_data[0].size(); x++) {
+				t_matrix[x][y] = _data[y][x];
+			}
+		}
+		return (t_matrix);
+	}
 	Matrix<Y, X> operator+(const Matrix<Y, X>& other) const
 	{
 		Matrix<Y, X> result(*this);
@@ -48,10 +74,40 @@ public:
 		}
 		return result;
 	}
+	typename std::array<std::array<int, X>, Y>::reference operator[](size_t n)
+	{
+		return(_data[n]);
+	}
+	template<size_t T>
+	Matrix<Y, T> operator*(Matrix<X, T> other) const
+	{
+		Matrix<Y, T> result;
+		// std::cout << "Resuls[" << Y << "][" << T << "]" << std::endl;
+		for (size_t res_x = 0; res_x < T; res_x++)
+		{
+			for (size_t res_y = 0; res_y < Y; res_y++)
+			{
+				double sum = 0;
+				for (size_t sum_it = 0; sum_it < X; sum_it++)
+				{
+					// std::cout << "Lets multiply: \n";
+					// std::cout << _data[res_y][sum_it] << " * " << other[sum_it][res_x] << " (_data[" << res_y << "][" << sum_it << "]) * (other[" << sum_it << "][" << res_x << "])" << std::endl;
+					// std::cout << _data[res_y][sum_it] * other[sum_it][res_x] << std::endl;
+					sum += _data[res_y][sum_it] * other[sum_it][res_x];
+					// std::cout << "SUM is now " << sum << std::endl;
+				}
+				// std::cout << "Resuls[" << res_x << "][" << res_y << "] = " << sum <<std::endl;
+				result[res_y][res_x] = sum;
+			}
+		}
+		return result;
+	}
 	// Destructor
 	~Matrix(void) = default;
 	typename std::array<std::array<int, X>, Y>::iterator begin() {return _data.begin();}
 	typename std::array<std::array<int, X>, Y>::iterator end() {return _data.end();}
+	typename std::array<std::array<int, X>, Y>::const_iterator cbegin() {return _data.cbegin();}
+	typename std::array<std::array<int, X>, Y>::const_iterator cend() {return _data.cend();}
 protected:
 private:
 	rows_of_columns	_data;
--- main.cpp
+++ main.cpp
@@ -8,11 +8,29 @@ int main(void)
     Matrix<3, 3> B = {{0, 2, 4},
                       {2, 5, 3},
                       {2, 1, 4}};
-    Matrix<3, 4> C = {{0, 2, 4, 4},
-                      {2, 5, 3, 4},
-                      {2, 1, 4, 4}};
-    Matrix good_sum = A+B;
+    Matrix<3, 4> C = {{13, 9, 7, 15},
+                      {8, 7, 4, 6},
+                      {6, 4, 0, 3}};
+    Matrix<3, 3> good_sum = A+B;
     std::cout << "Matrix sum" << std::endl;
     std::cout << good_sum;
     // Matrix<3, 3> bad_sum = A+C;
+
+    std::cout << "Transposed" << std::endl;
+   Matrix<4, 3> D = C.transposed();
+    std::cout << D;
+
+    Matrix<1, 3> E {{3, 4, 2}};
+    Matrix<3, 4> F(C);
+    std::cout << "Multiplying E\n" << E
+    << " by C\n" << C << "Returns\n";
+    Matrix<1, 4> G = E * C;
+    std::cout << G;
+    Matrix<2, 3> H = {{1, 2, 3},
+                      {4, 5, 6}};
+    Matrix<3, 2> I = {{7, 8},
+                      {9, 10},
+                      {11, 12}};
+    Matrix<2, 2> J = H * I;
+    std::cout << J;
 }
